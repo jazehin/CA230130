634309a92f3cf8f47b377b41340b04a90b7c8228
kiss.bence2 <kiss.bence2@28-05>
2023-01-30 08:35:58 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+string password = "Asdfgh123";
+
+using SHA256 sha256 = SHA256.Create();
+
+byte[] buffer = Encoding.ASCII.GetBytes(password);
+
+byte[] hashValue = sha256.ComputeHash(buffer);
+
+string hvs = String.Empty;
+for (int i = 0; i < hashValue.Length; i++)
+{
+    hvs += $"{hashValue[i]:X2}";
+    if ((i+1) % 4 == 0) hvs += " ";
+}
+
+Console.WriteLine($"input:  {password}");
+Console.WriteLine($"output: {hvs}");
